a08e2c5fc11347f323a139f962b400daa916e70e
Gergely Nemeth <neged.ng@gmail.com>
2023-05-05 14:23:33 +0200

    more exp

--- exp/setups.py
+++ exp/setups.py
@@ -9,7 +9,7 @@ def get_experiment(exp_name='default', params=""):
     if exp_name == 'default':
         return exp_default()
     if exp_name == "unit_size":
-        return exp_unit_size2()
+        return exp_unit_size3()
     if exp_name == "scale_us":
         return exp_scale_us3()
     if exp_name == "minus_one":
@@ -55,6 +55,17 @@ def exp_unit_size2():
         conf_changes.append(c)
     return conf_changes
 
+def exp_unit_size3():
+    conf_changes = []
+    l = list(range(2,40,4))
+    for s in l:
+        c = {"unit_size" : s}
+        c['ma_mode'] = 'no'
+        c['split_mode'] = 'homogen'
+        c['scale_mode'] = 1.0
+        c['rounds'] = 70
+        conf_changes.append(c)
+    return conf_changes
 
 def exp_unit_size_minus_one():
     conf_changes = []
