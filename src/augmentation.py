36cf2e04586fa243f9453f8eefd3b4d83ca5ae25
Gergely Nemeth <neged.ng@gmail.com>
2023-05-30 15:39:06 +0200

    fix augmentation and change default to cifar10 default

--- src/augmentation.py
+++ src/augmentation.py
@@ -1,3 +1,6 @@
+# Default parameters to match: https://github.com/diaoenmao/HeteroFL-Computation-and-Communication-Efficient-Federated-Learning-for-Heterogeneous-Clients/blob/master/src/data.py
+# and https://github.com/bearpaw/pytorch-classification/blob/master/cifar.py
+
 import tensorflow as tf
 
 def aug_crop(image, conf):
@@ -7,9 +10,16 @@ def aug_crop(image, conf):
     image_p = tf.pad(image, paddings, "CONSTANT")
     image_c = tf.image.random_crop(image_p,orig_shape)
         
-    return image
+    return image_c
+
+
+def aug_horizontal_flip(image):
+    return tf.image.random_flip_left_right(image)
+
     
 def aug_ds(image, label, conf):
     if conf["aug_crop"]>0:
         image = aug_crop(image, conf)
+    if conf["aug_horizontal_flip"]:
+        image = aug_horizontal_flip(image)
     return image, label
