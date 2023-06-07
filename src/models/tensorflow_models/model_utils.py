7900b4dec834159fae6942ebca215884b4754a3f
Gergely Nemeth <neged.ng@gmail.com>
2023-06-07 10:55:54 +0200

    TF to allocate memory to process only if needed

--- src/models/tensorflow_models/model_utils.py
+++ src/models/tensorflow_models/model_utils.py
@@ -4,7 +4,19 @@ import numpy as np
 
 from src.models.tensorflow_models import scaler, diao_cnn, alexnet, simple_cnn, resnet
 
-print(tf.config.list_physical_devices('GPU'))
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+  try:
+    # Currently, memory growth needs to be the same across GPUs
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+    logical_gpus = tf.config.list_logical_devices('GPU')
+    # print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Memory growth must be set before GPUs have been initialized
+    print(e)
+
+
 
 custom_objects = {"Scaler": scaler.Scaler}
 
